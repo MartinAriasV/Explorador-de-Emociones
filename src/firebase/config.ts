7165869184ee2c235431404db3aee88bb917faaf
Martin Arias <m4rtin.4ri4s@gmail.com>
2025-11-03 06:12:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2040032983-134ea",
+  "appId": "1:851535225513:web:1dc9ce8936f7087d01ffb8",
+  "apiKey": "AIzaSyBaMToK8ZZPiqP12dncGaedynE2JeiINCQ",
+  "authDomain": "studio-2040032983-134ea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "851535225513"
+};
